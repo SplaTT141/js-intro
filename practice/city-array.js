0d1e8bcc3752db062696eac11ae8d4b3de1b3baf
Ernest <zyperis14@gmail.com>
2025-04-02 15:00:33 +0300

    practise

--- practice/city-array.js
+++ practice/city-array.js
@@ -1,24 +1,27 @@
 const city = [
-    'Kaunas', 89.3,
-    'Riga', 284.7,
     'Tallinn', 600,
+    'Minsk', 194.5,
+    'Riga', 284.7,
+    'Kaunas', 89.3,
     'Warsaw', 450,
     'Berlin', 820.4,
     'Stockholm', 660.8,
     'Kyiv', 590,
     'Moscow', 990,
-    'Minsk', 194.5,
     'Amsterdam', 1400.6
 ];
 
 function travelTime(speed, time) {
     let travelRange = speed * time;
 
-    for (i = 0; i < city.length; i += 2) {
-        const distance = city[i+1];
-        console.log(distance)
+    for (i = 0; i < city.length; i += 2) {       
+        const cityName = city[i];
+        const distance = city[i + 1];
+        
+        if (distance <= travelRange) {
+            console.log(cityName + ' ' + distance);
+        }
     }
-
 }
 
 console.log(travelTime(200, 2))
