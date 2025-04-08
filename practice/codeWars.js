d98ee472aa1fbaf64ccc4ef670726ee6be3c2ff4
Ernest <zyperis14@gmail.com>
2025-04-08 15:58:09 +0300

    codewars

--- practice/codeWars.js
+++ practice/codeWars.js
@@ -24,11 +24,87 @@ function validatePIN (pin) {
 
     console.log(validatePIN("123 "),)
 
-console.log('-----------')
-const a = 0.25
-const b = .25
-const c = 125
+console.log('-----------');
 
-console.log(Number.isInteger(a));
-console.log(Number.isInteger(b));
-console.log(Number.isInteger(c));
+function DNAtoRNA(dna) {
+    if (dna.includes('T')) {
+        return dna.replaceAll('T', 'U')
+    }
+}
+
+console.log(DNAtoRNA('TTT'))
+
+console.log('-----------');
+
+function greet(language) {
+	if (language === 'english') {
+    return 'Welcome';
+  }
+  	if (language === 'czech') {
+    return 'Vitejte';
+  }
+  	if (language === 'danish') {
+    return 'Velkomst';
+  }
+  	if (language === 'dutch') {
+    return 'Welkom';
+  }
+  	if (language === 'estonian') {
+    return 'Tere tulemast';
+  }
+  	if (language === 'finnish') {
+    return 'Tervetuloa';
+  }
+  	if (language === 'flemish') {
+    return 'Welgekomen';
+  }
+  	if (language === 'german') {
+    return 'Willkommen';
+  }
+  	if (language === 'irish') {
+    return 'Failte';
+  }
+  	if (language === 'italian') {
+    return 'Benvenuto';
+  }
+    	if (language === 'latvian') {
+    return 'Gaidits';
+  }
+    	if (language === 'lithuanian') {
+    return 'Laukiamas';
+  }
+    	if (language === 'polish') {
+    return 'Witamy';
+  }
+    	if (language === 'spanish') {
+    return 'Bienvenido';
+  }
+      	if (language === 'swedish') {
+    return 'Valkommen';
+  }
+      	if (language === 'welsh') {
+    return 'Croeso';
+  }
+}
+
+console.log(greet('english'))
+
+console.log('-----------');
+
+function smash (words) {
+    return words.toString().replaceAll(',', ' ');
+};
+
+
+console.log(smash(['hello', 'world']));
+
+console.clear();
+console.log('-----------');
+
+function stringClean(s){
+    return s.replaceAll(/1|2|3|4|5|6|7|8|9|0/ig, '');     
+}
+  console.log(stringClean('12345698'))
+  console.log(stringClean('a12b13c'))
+
+  console.log('-----------');
