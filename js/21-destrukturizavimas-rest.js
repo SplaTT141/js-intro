c7c293e7a89e27e0b652671db6fba44a08ba6266
Ernest <zyperis14@gmail.com>
2025-04-10 09:37:09 +0300

    destruktorizavimas

--- js/21-destrukturizavimas-rest.js
+++ js/21-destrukturizavimas-rest.js
@@ -64,13 +64,81 @@ console.log(x);
 console.log(y);
 console.log(z);
 
+const mix = [1, 'asd', true, [], {}, undefined];
+const [...mAll] = mix; // suzinom visus masyvo mix narius
+
+console.log(mAll);
+
+console.clear();
+
+const person = {
+    address: 'Miestas g 7',
+    name: 'Jonas',
+    age: 99,
+    isMarried: true,
+};
+const {name, age, ...p} = person; //objektai neturi eiliskumo, todel reikia nurodyti objekto rakto pavadinima.
+
+console.log(name);
+console.log(age);
+console.log(p);
+console.log(p.address);
+
+const {address, ...pp} = p
+console.log(address);
+console.log(pp);
+
+console.clear();
+
+const students = [
+    {name: 'Jonas', age: 99},
+    {name: 'Maryte', age: 88},
+    {name: 'Petras', age: 77},
+    {name: 'Ona', age: 66},
+]
+
+// Studentui, vardu Jonas, yra 99 metai.
 
 
+for (let i = 0; i < students.length; i++) {
+    const {name, age} = students[i];
+    const result = `Studentui, vardu ${name}, yra ${age} metai,`
+    console.log(result);
+}
 
+console.log('----------');
 
+for (const student of students) {
+    const {name, age} = student;
+    const result = `Studentui, vardu ${name}, yra ${age} metai,`
+    console.log(result);
+}
 
+console.log('----------');
 
+for (const {name, age} of students) {
+    const result = `Studentui, vardu ${name}, yra ${age} metai,`
+    console.log(result);
+}
 
+console.clear();
+
+const numbers = [
+    [0],
+    [1, 11],
+    [2, 22, 222],
+    [3, 33, 333, 3333],
+    [4],
+    [5],
+    [6],
+];
+
+// Pirmas skaicius yra X ir dar yra Y papildomu skaiciu
+
+for (const [first, ...rest] of numbers) {    
+    const result = `Pirmas skaicius yra ${first} ir dar yra ${rest.length} papildomu skaiciu`
+    console.log(result)
+}
 
 
 
