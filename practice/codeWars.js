7a3d8594088bc718ce4a3b469acbedc400ec9c22
Ernest <zyperis14@gmail.com>
2025-04-11 15:10:22 +0300

    up

--- practice/codeWars.js
+++ practice/codeWars.js
@@ -1,171 +1,229 @@
 // reikia patikrinti ar visi zenklai yra skaiciai
-// reikia patikrinti ar skaiciu yra 4 arba 6  
+// reikia patikrinti ar skaiciu yra 4 arba 6
 
-
-function validatePIN (pin) {
-
-    if (isNaN(Number(pin))) {
-        return false;
-    }
-    if (pin < 0) {
-        return false;
-    }
-    if (pin.includes('.') || pin.includes('\n')) {
-        return false;
-    }
-    if (pin.length === 4 || pin.length === 6) {
-        return true;
-    } else {
-        return false;
-    }
-
-    return 0;
+function validatePIN(pin) {
+  if (isNaN(Number(pin))) {
+    return false;
+  }
+  if (pin < 0) {
+    return false;
+  }
+  if (pin.includes(".") || pin.includes("\n")) {
+    return false;
+  }
+  if (pin.length === 4 || pin.length === 6) {
+    return true;
+  } else {
+    return false;
   }
 
-    console.log(validatePIN("123 "),)
+  return 0;
+}
 
-console.log('-----------');
+console.log(validatePIN("123 "));
+
+console.log("-----------");
 
 function DNAtoRNA(dna) {
-    if (dna.includes('T')) {
-        return dna.replaceAll('T', 'U')
-    }
+  if (dna.includes("T")) {
+    return dna.replaceAll("T", "U");
+  }
 }
 
-console.log(DNAtoRNA('TTT'))
+console.log(DNAtoRNA("TTT"));
 
-console.log('-----------');
+console.log("-----------");
 
 function greet(language) {
-	if (language === 'english') {
-    return 'Welcome';
+  if (language === "english") {
+    return "Welcome";
   }
-  	if (language === 'czech') {
-    return 'Vitejte';
+  if (language === "czech") {
+    return "Vitejte";
   }
-  	if (language === 'danish') {
-    return 'Velkomst';
+  if (language === "danish") {
+    return "Velkomst";
   }
-  	if (language === 'dutch') {
-    return 'Welkom';
+  if (language === "dutch") {
+    return "Welkom";
   }
-  	if (language === 'estonian') {
-    return 'Tere tulemast';
+  if (language === "estonian") {
+    return "Tere tulemast";
   }
-  	if (language === 'finnish') {
-    return 'Tervetuloa';
+  if (language === "finnish") {
+    return "Tervetuloa";
   }
-  	if (language === 'flemish') {
-    return 'Welgekomen';
+  if (language === "flemish") {
+    return "Welgekomen";
   }
-  	if (language === 'german') {
-    return 'Willkommen';
+  if (language === "german") {
+    return "Willkommen";
   }
-  	if (language === 'irish') {
-    return 'Failte';
+  if (language === "irish") {
+    return "Failte";
   }
-  	if (language === 'italian') {
-    return 'Benvenuto';
+  if (language === "italian") {
+    return "Benvenuto";
   }
-    	if (language === 'latvian') {
-    return 'Gaidits';
+  if (language === "latvian") {
+    return "Gaidits";
   }
-    	if (language === 'lithuanian') {
-    return 'Laukiamas';
+  if (language === "lithuanian") {
+    return "Laukiamas";
   }
-    	if (language === 'polish') {
-    return 'Witamy';
+  if (language === "polish") {
+    return "Witamy";
   }
-    	if (language === 'spanish') {
-    return 'Bienvenido';
+  if (language === "spanish") {
+    return "Bienvenido";
   }
-      	if (language === 'swedish') {
-    return 'Valkommen';
+  if (language === "swedish") {
+    return "Valkommen";
   }
-      	if (language === 'welsh') {
-    return 'Croeso';
+  if (language === "welsh") {
+    return "Croeso";
   }
 }
 
-console.log(greet('english'))
+console.log(greet("english"));
 
-console.log('-----------');
+console.log("-----------");
 
-function smash (words) {
-    return words.toString().replaceAll(',', ' ');
-};
+function smash(words) {
+  return words.toString().replaceAll(",", " ");
+}
 
-
-console.log(smash(['hello', 'world']));
+console.log(smash(["hello", "world"]));
 
 console.clear();
-console.log('-----------');
+console.log("-----------");
 
-function stringClean(s){
-    return s.replaceAll(/1|2|3|4|5|6|7|8|9|0/ig, '');     
+function stringClean(s) {
+  return s.replaceAll(/1|2|3|4|5|6|7|8|9|0/gi, "");
 }
-  console.log(stringClean('12345698'))
-  console.log(stringClean('a12b13c'))
+console.log(stringClean("12345698"));
+console.log(stringClean("a12b13c"));
 
-  console.log('-----------');
+console.log("-----------");
 
-  function abbrevName(name){
+function abbrevName(name) {
+  const a = name.split(" ");
+  const firstNameAbb = a[0][0];
+  const secondNameAbb = a[1][0];
 
-    const a = name.split(' ')
-    const firstNameAbb = a[0][0]
-    const secondNameAbb = a[1][0]
-    
-    return firstNameAbb.toUpperCase() + '.' + secondNameAbb.toUpperCase() 
-  }
-
-  console.log(abbrevName('Ernest Ivanov'))
-
-console.log('-----------');
-
-function noSpace(x){
-  return x.replaceAll(' ', '');
+  return firstNameAbb.toUpperCase() + "." + secondNameAbb.toUpperCase();
 }
 
-console.log(noSpace('8aaaaa dddd r     ')) 
+console.log(abbrevName("Ernest Ivanov"));
 
-console.log('-----------');
+console.log("-----------");
+
+function noSpace(x) {
+  return x.replaceAll(" ", "");
+}
+
+console.log(noSpace("8aaaaa dddd r     "));
+
+console.log("-----------");
 
 function disemvowel(str) {
-  return str.replaceAll(/a|e|i|o|u/ig, '');
+  return str.replaceAll(/a|e|i|o|u/gi, "");
 }
 
-console.log(disemvowel("This website is for losers LOL!"))
+console.log(disemvowel("This website is for losers LOL!"));
 //Ths wbst s fr lsrs LL!
 
-console.log('-----------');
+console.log("-----------");
 
 function dnaStrand(dna) {
-  return dna.split('').map(base => {
-    if (base === 'A') return 'T';
-    if (base === 'T') return 'A';
-    if (base === 'C') return 'G';
-    if (base === 'G') return 'C';
-  }).join('');
+  return dna
+    .split("")
+    .map((base) => {
+      if (base === "A") return "T";
+      if (base === "T") return "A";
+      if (base === "C") return "G";
+      if (base === "G") return "C";
+    })
+    .join("");
 }
 
-console.log(dnaStrand('ATTGC')) // TAACG
-console.log(dnaStrand('AAAA'))
+console.log(dnaStrand("ATTGC")); // TAACG
+console.log(dnaStrand("AAAA"));
 
-console.log('-----------');
-
+console.log("-----------");
 
 function century(year) {
   if (year % 100 === 0) {
-    return Math.floor(year / 100)
+    return Math.floor(year / 100);
   }
-    return Math.floor(year / 100 + 1);
+  return Math.floor(year / 100 + 1);
+}
+
+console.log(century(1234));
+console.log(century(1900));
+console.log(century(2025));
+console.log(century(101));
+console.log(century(2));
+
+console.log("-----------");
+console.clear();
+
+function maps(x) {
+  let doubleX = 0;
+  let list = [];
+  for (i = 0; i < x.length; i++) {
+    doubleX = x[i] * 2;
+    console.log(doubleX);
+    list.push(doubleX);
   }
-  
-console.log(century(1234))
-console.log(century(1900))
-console.log(century(2025))
-console.log(century(101))
-console.log(century(2))
+  return list;
+}
 
-console.log('-----------');
+console.log(maps([1, 2, 3]));
 
+console.log("-----------");
+
+function invert(array) {
+  let reversedNums = [];
+  for (i = 0; i < array.length; i++) {
+    reversedNums.push(array[i] * -1);
+  }
+  return reversedNums;
+}
+
+console.log(invert([1, -2, 3, -4, 5]));
+
+const arrowFunc = function (arr) {
+  return arr.map((el) => String.fromCharCode(el)).join("");
+};
+
+console.log(arrowFunc([84, 101, 115, 116, 75]));
+
+console.log("-----------");
+
+const booleanToString = (b) => (b ? "true" : "false");
+
+// function booleanToString(b) {
+//   if (b === true) {
+//     return "true";
+//   } else {
+//     return "false";
+//   }
+// }
+console.log(booleanToString(false));
+
+console.log("-----------");
+
+function findSmallestInt(arr) {
+  let arr1 = [];
+  for (i = 0; i < arr.length; i++) {
+    arr1.push(arr[i]);
+    console.log(arr1);
+  }
+  let arr2 = arr1.join(", ");
+  let arr3 = Number(arr2);
+  console.log(typeof arr2);
+  console.log(typeof arr3);
+}
+
+console.log(findSmallestInt([78, 56, 232, 12, 8]));
